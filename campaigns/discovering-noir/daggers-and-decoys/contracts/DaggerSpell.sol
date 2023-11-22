554c73e3bab8fcb7f01b8b9cf414acb98c6ac378
bryanwee023 <72504548+bryanwee023@users.noreply.github.com>
2023-11-22 11:51:20 +0000

    Deploy new Quests 699398

--- campaigns/discovering-noir/daggers-and-decoys/contracts/DaggerSpell.sol
+++ campaigns/discovering-noir/daggers-and-decoys/contracts/DaggerSpell.sol
@@ -7,5 +7,34 @@ contract DaggerSpell is IDaggerSpell {
     
     constructor(address[8] memory _keepers) { }
 
-}
+    /// @inheritdoc IDaggerSpell
+    function merkleLeaf(uint256 _index) external view returns (bytes32) {
 
+    }
+
+    /// @inheritdoc IDaggerSpell
+    function merkleRoot() external view returns (bytes32) {
+
+    }
+
+    /// @inheritdoc IDaggerSpell
+    function daggers(address _keeper) external view returns (uint256) {
+
+    }
+
+    /// @inheritdoc IDaggerSpell
+    function giveDagger(bytes32 _merkleLeaf) external {
+
+    }
+
+    /// @inheritdoc IDaggerSpell
+    function computeRoot() external {
+
+    }
+
+    /// @inheritdoc IDaggerSpell
+    function pullDagger(bytes32 _nullifier, bytes calldata _proof) external {
+
+    }
+
+}
